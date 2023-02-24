1260a6398642f2c188300fde07284ac6449c5490
Jonathan McCaffrey <jonmcjon@hotmail.com>
2023-02-24 15:03:29 -0500

    added queries, CTEs, views

--- data/Account_balances.sql
+++ data/Account_balances.sql
@@ -2,12 +2,12 @@ Use DHHS_project
 
 -- testing csv upload
 SELECT *
-FROM [FY2020Q1-P12_All_FA_AccountBalances]
+FROM [FY2020Q1-P12_All_FA_AccountBalances];
 
 -- calculating gross outlay by year and organized by federal account name
 SELECT a.gross_outlay_amount AS "2020_gross"
-	,b.gross_outlay_amount AS "2021_gross"
-	,c.gross_outlay_amount AS "2022_gross"
+	, b.gross_outlay_amount AS "2021_gross"
+	, c.gross_outlay_amount AS "2022_gross"
 	,(a.gross_outlay_amount + b.gross_outlay_amount + c.gross_outlay_amount) AS "combined_gross"
 	, a.federal_account_name
 FROM [FY2020Q1-P12_All_FA_AccountBalances] a
@@ -15,13 +15,61 @@ FROM [FY2020Q1-P12_All_FA_AccountBalances] a
 	ON a.federal_account_name = b.federal_account_name
 	INNER JOIN [FY2022P01-P12_All_FA_AccountBalances] c
 	ON a.federal_account_name = c.federal_account_name
-ORDER BY combined_gross DESC
+ORDER BY combined_gross DESC;
 
--- Investigating "budget_authority_appropriated_amount"
-SELECT budget_authority_appropriated_amount
-	, gross_outlay_amount
-	, federal_account_name
-FROM [FY2020Q1-P12_All_FA_AccountBalances]
-WHERE budget_authority_appropriated_amount != 0
+-- Creating a view of the above query for later visualization
+CREATE VIEW CombGross AS
+SELECT a.gross_outlay_amount AS "2020_gross"
+	, b.gross_outlay_amount AS "2021_gross"
+	, c.gross_outlay_amount AS "2022_gross"
+	,(a.gross_outlay_amount + b.gross_outlay_amount + c.gross_outlay_amount) AS "combined_gross"
+	, a.federal_account_name
+FROM [FY2020Q1-P12_All_FA_AccountBalances] a
+	INNER JOIN [FY2021P01-P12_All_FA_AccountBalances] b 
+	ON a.federal_account_name = b.federal_account_name
+	INNER JOIN [FY2022P01-P12_All_FA_AccountBalances] c
+	ON a.federal_account_name = c.federal_account_name;
 
--- Yields 89 rows (eliminates ~76% of rows) when removing zeroes
+SELECT *
+FROM CombGross
+ORDER BY combined_gross DESC;
+
+-- Investigating "budget_authority_appropriated_amount" and "combined outlay"
+SELECT a.federal_account_name
+	, a.budget_authority_appropriated_amount AS "2020_budget"
+	, b.budget_authority_appropriated_amount AS "2021_budget"
+	, c.budget_authority_appropriated_amount AS "2022_budget"
+	, (a.budget_authority_appropriated_amount + b.budget_authority_appropriated_amount + c.budget_authority_appropriated_amount) AS "combined_budget"
+	, a.gross_outlay_amount AS "2020_outlay"
+	, b.gross_outlay_amount AS "2021_outlay"
+	, c.gross_outlay_amount AS "2022_outlay"
+	, (a.gross_outlay_amount + b.gross_outlay_amount + c.gross_outlay_amount) AS "combined_outlay"
+FROM [FY2020Q1-P12_All_FA_AccountBalances] a
+	INNER JOIN [FY2021P01-P12_All_FA_AccountBalances] b
+	ON a.federal_account_name = b.federal_account_name
+	INNER JOIN [FY2022P01-P12_All_FA_AccountBalances] c
+	ON a.federal_account_name = c.federal_account_name
+WHERE a.budget_authority_appropriated_amount != 0
+ORDER BY a.federal_account_name;
+
+-- Creating a view of the above query as well
+CREATE VIEW CombBudgOutlay AS 
+SELECT a.federal_account_name
+	, a.budget_authority_appropriated_amount AS "2020_budget"
+	, b.budget_authority_appropriated_amount AS "2021_budget"
+	, c.budget_authority_appropriated_amount AS "2022_budget"
+	, (a.budget_authority_appropriated_amount + b.budget_authority_appropriated_amount + c.budget_authority_appropriated_amount) AS "combined_budget"
+	, a.gross_outlay_amount AS "2020_outlay"
+	, b.gross_outlay_amount AS "2021_outlay"
+	, c.gross_outlay_amount AS "2022_outlay"
+	, (a.gross_outlay_amount + b.gross_outlay_amount + c.gross_outlay_amount) AS "combined_outlay"
+FROM [FY2020Q1-P12_All_FA_AccountBalances] a
+	INNER JOIN [FY2021P01-P12_All_FA_AccountBalances] b
+	ON a.federal_account_name = b.federal_account_name
+	INNER JOIN [FY2022P01-P12_All_FA_AccountBalances] c
+	ON a.federal_account_name = c.federal_account_name
+WHERE a.budget_authority_appropriated_amount != 0;
+
+SELECT *
+FROM CombBudgOutlay
+ORDER BY combined_outlay DESC;
--- data/Account_breakdown_by_award.sql
+++ data/Account_breakdown_by_award.sql
@@ -1,74 +1,207 @@
-USE DHHS_project
+USE DHHS_project;
 
 -- testing csv upload
 SELECT *
-FROM [FY2020Q1-P12_All_FA_AccountBreakdownByAward]
+FROM [FY2020Q1-P12_All_FA_AccountBreakdownByAward];
 
 -- AWARDS BY STATE
 
 -- calculating total awards by state for 2020
-SELECT DISTINCT recipient_state
-	,COUNT(recipient_state) AS "2020_total_state_count"
+-- Example of temp table
+DROP TABLE IF EXISTS #StateCount2020
+CREATE TABLE #StateCount2020
+(
+State nvarchar(255),
+StateCount numeric
+)
+INSERT INTO #StateCount2020
+SELECT DISTINCT recipient_state AS State
+	,COUNT(recipient_state) AS StateCount
 FROM [FY2020Q1-P12_All_FA_AccountBreakdownByAward]
 GROUP BY recipient_state
-ORDER BY "2020_total_count" DESC
+
+SELECT *
+FROM #StateCount2020
+ORDER BY StateCount DESC;
+
+-- Opting to use CTE to be able to use in View / visualizations later
+-- and creating separate CTEs for later combination
+WITH Count2020 (State, StateCount)
+AS
+(
+SELECT DISTINCT recipient_state AS State
+	,COUNT(recipient_state) AS StateCount
+FROM [FY2020Q1-P12_All_FA_AccountBreakdownByAward]
+GROUP BY recipient_state
+)
+SELECT *
+FROM Count2020
+ORDER BY StateCount DESC;
+
 
 -- total awards by state for 2021
-SELECT DISTINCT(recipient_state)
-	, COUNT(recipient_state) AS "2021_total_state_count"
+WITH Count2021 (State, StateCount)
+AS
+(
+SELECT DISTINCT(recipient_state) AS State
+	, COUNT(recipient_state) AS StateCount
 FROM [FY2021P01-P12_All_FA_AccountBreakdownByAward]
 GROUP BY recipient_state
-ORDER BY"2021_total_count" DESC 
+)
+SELECT *
+FROM Count2021
+ORDER BY StateCount DESC;
 
 -- and total awards by state for 2022
-SELECT DISTINCT(recipient_state)
-	, COUNT(recipient_state) AS "2022_total_state_count"
+WITH Count2022 (State, StateCount)
+AS
+(
+SELECT DISTINCT(recipient_state) AS State
+	, COUNT(recipient_state) AS StateCount
 FROM [FY2022P01-P12_All_FA_AccountBreakdownByAward]
 GROUP BY recipient_state
-ORDER BY"2022_total_count" DESC
+)
+SELECT *
+FROM Count2022
+ORDER BY StateCount DESC;
+
+-- Saving as a view for later visualization and to calculate totals
+CREATE VIEW PerStateCount AS
+(
+SELECT DISTINCT recipient_state AS State
+	,COUNT(recipient_state) AS StateCount
+FROM [FY2020Q1-P12_All_FA_AccountBreakdownByAward]
+GROUP BY recipient_state
+)
+UNION
+(
+SELECT DISTINCT(recipient_state) AS State
+	, COUNT(recipient_state) AS StateCount
+FROM [FY2021P01-P12_All_FA_AccountBreakdownByAward]
+GROUP BY recipient_state
+)
+UNION
+(
+SELECT DISTINCT(recipient_state) AS State
+	, COUNT(recipient_state) AS StateCount
+FROM [FY2022P01-P12_All_FA_AccountBreakdownByAward]
+GROUP BY recipient_state
+);
+
+CREATE VIEW TotalStateCount
+AS
+(
+SELECT State, SUM(StateCount) AS total_count
+FROM PerStateCount
+GROUP BY State
+-- ORDER BY total_count DESC
+);
+
+SELECT *
+FROM TotalStateCount
+ORDER BY total_count DESC;
+
+
 
 -- AWARDS BY CONGRESSIONAL DISTRICT
 
--- calculating total awards by congressional district, for 2020
-SELECT DISTINCT recipient_congressional_district
-	, COUNT(recipient_congressional_district) AS "2020_total_cong_count"
-	, recipient_state
-	, recipient_county
-	, recipient_city
+-- calculating total awards by congressional district, for 2020 and creating CTE
+
+WITH ConDist2020 (ConDist, TotalConDistCount)
+AS
+(
+SELECT DISTINCT recipient_congressional_district AS ConDist
+	, COUNT(recipient_congressional_district) AS TotalConDistCount
 FROM [FY2020Q1-P12_All_FA_AccountBreakdownByAward]
-GROUP BY recipient_congressional_district, recipient_state, recipient_county, recipient_city
-ORDER BY "2020_total_cong_count" DESC
+GROUP BY recipient_congressional_district
+)
+SELECT *
+FROM ConDist2020
+ORDER BY TotalConDistCount DESC;
+
 
 -- and for 2021
-SELECT DISTINCT recipient_congressional_district
-	, COUNT(recipient_congressional_district) AS "2021_total_cong_count"
-	, recipient_state
-	, recipient_county
-	, recipient_city
+
+WITH ConDist2021 (ConDist, TotalConDistCount)
+AS
+(
+SELECT DISTINCT recipient_congressional_district AS ConDist
+	, COUNT(recipient_congressional_district) AS TotalConDistCount
 FROM [FY2021P01-P12_All_FA_AccountBreakdownByAward]
-GROUP BY recipient_congressional_district, recipient_state, recipient_county, recipient_city
-ORDER BY "2021_total_cong_count" DESC
+GROUP BY recipient_congressional_district
+)
+SELECT *
+FROM ConDist2021
+ORDER BY TotalConDistCount DESC;
 
 -- also for 2022
-SELECT DISTINCT recipient_congressional_district
-	, COUNT(recipient_congressional_district) AS "2022_total_cong_count"
-	, recipient_state
-	, recipient_county
-	, recipient_city
+WITH ConDist2022 (ConDist, TotalConDistCount)
+AS
+(
+SELECT DISTINCT recipient_congressional_district AS ConDist
+	, COUNT(recipient_congressional_district) AS TotalConDistCount
 FROM [FY2022P01-P12_All_FA_AccountBreakdownByAward]
-GROUP BY recipient_congressional_district, recipient_state, recipient_county, recipient_city
-ORDER BY "2022_total_cong_count" DESC
+GROUP BY recipient_congressional_district
+)
+SELECT *
+FROM ConDist2022
+ORDER BY TotalConDistCount DESC;
 
---SELECT COUNT(a.recipient_state) + COUNT(b.recipient_state) + COUNT(c.recipient_state) AS total_count
---FROM [FY2020Q1-P12_All_FA_AccountBreakdownByAward] a
---	JOIN [FY2021P01-P12_All_FA_AccountBreakdownByAward] b
---	ON a.recipient_state = b.recipient_state
---	JOIN [FY2022P01-P12_All_FA_AccountBreakdownByAward] c
---	ON a.recipient_state = c.recipient_state
---GROUP BY a.recipient_state
---ORDER BY total_count DESC
 
--- GROSS OUTLAY AMOUNT in these tables
+-- Creating a view for separate congressional districts
+CREATE VIEW PerConDist AS
+(
+SELECT DISTINCT recipient_congressional_district AS ConDist
+	, COUNT(recipient_congressional_district) AS TotalConDistCount
+FROM [FY2020Q1-P12_All_FA_AccountBreakdownByAward]
+GROUP BY recipient_congressional_district
+)
+UNION
+(
+SELECT DISTINCT recipient_congressional_district AS ConDist
+	, COUNT(recipient_congressional_district) AS TotalConDistCount
+FROM [FY2021P01-P12_All_FA_AccountBreakdownByAward]
+GROUP BY recipient_congressional_district
+)
+UNION
+(
+SELECT DISTINCT recipient_congressional_district AS ConDist
+	, COUNT(recipient_congressional_district) AS TotalConDistCount
+FROM [FY2022P01-P12_All_FA_AccountBreakdownByAward]
+GROUP BY recipient_congressional_district
+);
+
+-- And creating a view for totals
+CREATE VIEW TotalConDist
+AS
+(
+SELECT ConDist
+	, SUM(TotalConDistCount) AS total_count
+FROM PerConDist
+GROUP BY ConDist
+-- ORDER BY total_count DESC
+);
+
+SELECT DISTINCT ConDist
+	, total_count
+FROM TotalConDist
+ORDER BY total_count DESC;
+
+-- JOIN back to 2020 Account Breakdown by Award
+-- for whatever reason, it lists the same recipient_state's across multiple recipient_congressional_districts
+-- see below for 'PA' as an example
+-- I double-checked by sorting in Excel too.  
+SELECT a.recipient_state
+	, b.ConDist
+	, b.total_count
+FROM [FY2020Q1-P12_All_FA_AccountBreakdownByAward] a
+	JOIN TotalConDist b
+	ON a.recipient_congressional_district = b.ConDist
+WHERE recipient_state = 'PA'
+ORDER BY total_count DESC;
+
+
+-- GROSS OUTLAY AMOUNT
 
 -- 2020
 SELECT gross_outlay_amount_FYB_to_period_end AS total_FYB
@@ -91,21 +224,14 @@ FROM [FY2022P01-P12_All_FA_AccountBreakdownByAward]
 WHERE gross_outlay_amount_FYB_to_period_end IS NOT NULL
 ORDER BY total_FYB DESC
 
---SELECT a.gross_outlay_amount_FYB_to_period_end AS "2020_gross_FYB"
---	,b.gross_outlay_amount_FYB_to_period_end AS "2021_gross_FYB"
---	,c.gross_outlay_amount_FYB_to_period_end AS "2022_gross_FYB"
---	,(a.gross_outlay_amount_FYB_to_period_end + 
---		b.gross_outlay_amount_FYB_to_period_end + 
---		c.gross_outlay_amount_FYB_to_period_end) AS "combined_gross_FYB"
---	, a.federal_account_name
---	, b.federal_account_name
---	, c.federal_account_name
---FROM [FY2020Q1-P12_All_FA_AccountBreakdownByAward] a
---	INNER JOIN [FY2021P01-P12_All_FA_AccountBreakdownByAward] b
---	ON a.federal_account_name = b.federal_account_name
---	INNER JOIN [FY2022P01-P12_All_FA_AccountBreakdownByAward] c
---	ON a.federal_account_name = c.federal_account_name
---WHERE a.gross_outlay_amount_FYB_to_period_end IS NOT NULL
---	AND b.gross_outlay_amount_FYB_to_period_end IS NOT NULL
---	AND c.gross_outlay_amount_FYB_to_period_end IS NOT NULL
---ORDER BY combined_gross_FYB DESC, a.federal_account_name
+
+-- Transaction Obligated Amount
+-- many NULLs across both gross_outlay... and total_obligation.  Didn't do anything with this
+SELECT DISTINCT federal_account_name
+	, SUM(transaction_obligated_amount) AS total_obligation
+	, gross_outlay_amount_FYB_to_period_end
+	, submission_period
+FROM [FY2020Q1-P12_All_FA_AccountBreakdownByAward]
+GROUP BY gross_outlay_amount_FYB_to_period_end, federal_account_name, submission_period
+ORDER BY submission_period
+
